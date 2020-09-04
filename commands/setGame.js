2b5e8dc22df30ba20e0b08366353320217ff59d6
Lazyy3549 <pierce3549@outlook.com>
2020-09-04 18:29:20 +0000

    added setGame command

---
+++ commands/setGame.js
@@ -0,0 +1,28 @@
+const oid = process.env.oid
+const botconfig = require("../botconfig.json");
+const prefix = botconfig.prefix
+const Discord = module.require('discord.js');
+
+module.exports.run = async (bot, message, args) => {
+if(!message.content.startsWith(prefix))return;
+ 
+ var game = args.join(' ')
+   
+   if(!message.member.hasPermission("ADMINISTRATOR")) return message.reply("You can't use that command!")
+   if (!game) {
+   
+    message.channel.send(`:ok_hand: Okay, I will set my activity back to normal!`)
+    bot.user.setActivity(`.help | ${bot.guilds.size} servers`, {type: "WATCHING"})
+  } else {
+    bot.user.setActivity(`${game}`, {type: "PLAYING"})
+    message.channel.send(`:ok_hand: Okay, I will set my activity to '${game}'!`)
+  
+  }
+}
+module.exports.config = {
+    name: "setGame",
+    description: "Sets bots current game",
+    usage: "?setGame",
+    accessableby: "Admins",
+    aliases: []
+}
