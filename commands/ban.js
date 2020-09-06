ed72ed6210a0e81279264f3d8318a4b591f0133b
Lazyy3549 <pierce3549@outlook.com>
2020-09-06 12:28:00 +0000

    updated commands

--- commands/ban.js
+++ commands/ban.js
@@ -1,20 +1,26 @@
-module.exports.run = async (bot, message, args) => {
-    if(!message.member.hasPermission('BAN_MEMBERS')) 
-        message.channel.send("You can't use that command");
-    else {
-        let bannedMember = await message.guild.members.ban(args);
-        if(bannedMember){
+const botsettings = require("../botsettings.json");
+const prefix = botsettings.prefix
+const Discord = module.require('discord.js');
 
-        try {
-            console.log(bannedMember.tag + " Got banned.");
-            message.channel.send (`${bannedMember} was banned from the server`)
-        }
-            catch(err) {
-            console.log(err);
-        }
-    }
-}
-}
+module.exports.run = async (bot, message, args) => {
+if(!message.content.startsWith(prefix))return;
+  
+  if (!message.member.hasPermission("BAN_MEMBERS")) return message.reply("Sorry, you don't have permissions to use this!");
+
+let member = message.mentions.members.first();
+if(!member)
+  return message.reply("Please mention a valid member of this server");
+if(!member.bannable) 
+  return message.reply("I cannot ban this user!")
+
+let reason = args.slice(1).join(' ');
+if(!reason) reason = "No reason provided";
+
+await member.ban(reason)
+  .catch(error => message.reply(`Sorry ${message.author} I couldn't ban because of : ${error}`));
+message.reply(`${member.user.tag} has been banned by ${message.author.tag} because: ${reason}`);
+  message.member.tag.sendMessage('Hi! You were banned from ${member.user.server} because: {reason}. If you do not get why you were banned, please DM @Xavier.')
+  }
 
 module.exports.config = {
     name: "ban",
