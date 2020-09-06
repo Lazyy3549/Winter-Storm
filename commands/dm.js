d84e648a28636f01a708e6967d3ef26f55b56430
Lazyy3549 <pierce3549@outlook.com>
2020-09-06 12:37:42 +0000

    updated dm command

--- commands/dm.js
+++ commands/dm.js
@@ -5,7 +5,7 @@ const Discord = module.require('discord.js');
 module.exports.run = async (bot, message, args) => {
 if(!message.content.startsWith(prefix))return;
 
-    let dUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);	
+    let dUser = message.guild.member(message.mentions.users.first()) || message.guild.members.fetch(args[0]);	
    if (!dUser) return message.channel.send("Can't find user!")	
    if(!message.member.hasPermission("ADMINISTRATOR")) return message.reply("You can't use that command!")	
    let dMessage = args.join(" ").slice(22);	
